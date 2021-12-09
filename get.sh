fc39c2103bc6ba1b5ee0253238d1d94c618ef929
lmc888 <85095031+lmc888@users.noreply.github.com>
2021-12-09 23:35:34 +0800

    Update get.sh

--- get.sh
+++ get.sh
@@ -2,8 +2,24 @@
 
 UA_Browser="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36"
 
-tmpresult=$(curl  --user-agent "${UA_Browser}" -4 -s --max-time 10 "https://apis.v-saas.com:9501/member/api/viewAuthorization?contentId=1&memberId=384030&menuId=3&platform=5&imei=c959b475-f846-4a86-8e9b-508048372407") 
-qq=$(echo $tmpresult | python -m json.tool 2> /dev/null | grep '"qq"' | cut -f4 -d'"')
-st=$(echo $tmpresult | python -m json.tool 2> /dev/null | grep '"st"' | cut -f4 -d'"')
-m3u_RUL=$(echo $tmpresult | python -m json.tool 2> /dev/null | grep boostStreamUrl | cut -f4 -d'"')
-echo "${m3u_RUL}?st=${st}&qq=${qq}" >> playurl.txt
+tmpresult1=$(curl  --user-agent "${UA_Browser}" -4 -s --max-time 10 "https://apis.v-saas.com:9501/member/api/viewAuthorization?contentId=1&memberId=384030&menuId=3&platform=5&imei=c959b475-f846-4a86-8e9b-508048372407") 
+qq1=$(echo $tmpresult | python -m json.tool 2> /dev/null | grep '"qq"' | cut -f4 -d'"')
+st1=$(echo $tmpresult | python -m json.tool 2> /dev/null | grep '"st"' | cut -f4 -d'"')
+m3u_url1=$(echo $tmpresult | python -m json.tool 2> /dev/null | grep boostStreamUrl | cut -f4 -d'"')
+ch01="${m3u_url1}?st=${st1}&qq=${qq1}"
+
+tmpresult2=$(curl  --user-agent "${UA_Browser}" -4 -s --max-time 10 "https://apis.v-saas.com:9501/member/api/viewAuthorization?contentId=2&memberId=384030&menuId=3&platform=5&imei=c959b475-f846-4a86-8e9b-508048372407")
+qq2=$(echo $tmpresult | python -m json.tool 2> /dev/null | grep '"qq"' | cut -f4 -d'"')
+st2=$(echo $tmpresult | python -m json.tool 2> /dev/null | grep '"st"' | cut -f4 -d'"')
+m3u_url2=$(echo $tmpresult | python -m json.tool 2> /dev/null | grep boostStreamUrl | cut -f4 -d'"')
+ch02="${m3u_url2}?st=${st2}&qq=${qq2}"
+
+tmpresult3=$(curl  --user-agent "${UA_Browser}" -4 -s --max-time 10 "https://apis.v-saas.com:9501/member/api/viewAuthorization?contentId=10&memberId=384030&menuId=3&platform=5&imei=c959b475-f846-4a86-8e9b-508048372407")
+qq3=$(echo $tmpresult | python -m json.tool 2> /dev/null | grep '"qq"' | cut -f4 -d'"')
+st3=$(echo $tmpresult | python -m json.tool 2> /dev/null | grep '"st"' | cut -f4 -d'"')
+m3u_url3=$(echo $tmpresult | python -m json.tool 2> /dev/null | grep boostStreamUrl | cut -f4 -d'"')
+ch03="${m3u_url3}?st=${st3}&qq=${qq3}"
+
+sed -i "2c ${ch01}" Eleven.m3u
+sed -i "4c ${ch02}" Eleven.m3u
+sed -i "6c ${ch03}" Eleven.m3u
