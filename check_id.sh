0995cfc4ee849a8fd64540980fdd54137f6567f6
lmc888 <85095031+lmc888@users.noreply.github.com>
2021-12-11 14:45:30 +0800

    Update check_id.sh

--- check_id.sh
+++ check_id.sh
@@ -2,18 +2,12 @@
 
 check_status(){
 e=$(cat id.txt)
-until [[ "$status" == "true" ]]
+until [[ "$status" == "true" ]] || [[ "$e" -gt "999999" ]]
 do
-	sleep 1
-	status=$(curl -s "https://apis.v-saas.com:9501/member/api/viewAuthorization?contentId=2&memberId=${e}&menuId=3&platform=5&imei=c959b475-f846-4a86-8e9b-508048372407" | python -m json.tool 2> /dev/null | grep authorization | awk '{print $2}' | cut -f1 -d",")
 	((e=$e+1))
-	
-	if [[ "$e" -gt 999999 ]]
-		then 
-			echo "无法成功连上节点"
-			exit 0
-	fi	
-	
+	sleep 1
+	status=$(curl -s -4 --max-time 5 "https://apis.v-saas.com:9501/member/api/viewAuthorization?contentId=2&memberId=${e}&menuId=3&platform=5&imei=c959b475-f846-4a86-8e9b-508048372407" | python -m json.tool 2> /dev/null | grep authorization | awk '{print $2}' | cut -f1 -d",")
+		
 
 done
 }
